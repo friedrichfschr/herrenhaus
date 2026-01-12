871fa87d2ef11ec83591e2dae17154b1b5ab919f
friedrich.fschr <friedrich.riesel@gmail.com>
2026-01-12 17:40:58 +0100

    fix path redirects for netlify

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   plugins: [react(), tsconfigPaths(), tailwindcss()],
   server: {
-    allowedHosts: ["herrenhaus.fschr.me"],
+    allowedHosts: ["herrenhaus.fschr.me, www.herrenhaus-fischer.de"],
   },
+  base: "/",
 });
